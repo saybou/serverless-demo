0115f8665088dc3d1f37fe18e679f6079fdb4c8e
Erwan <erwan.allair.ext@planity.com>
2024-07-01 12:54:35 +0200

    feat(payment): add getting stipe api key

--- services/payment/functions/handlePayment/config.ts
+++ services/payment/functions/handlePayment/config.ts
@@ -17,6 +17,13 @@ export default {
       Resource: { 'Fn::GetAtt': ['receiptTable', 'Arn'] },
       Action: ['dynamodb:PutItem'],
     },
+    {
+      Effect: 'Allow',
+      Resource: [
+        'arn:aws:ssm:${aws:region}:${aws:accountId}:parameter${self:provider.environment.STRIPE_API_KEY_SSM_PATH}',
+      ],
+      Action: ['ssm:GetParameters'],
+    },
   ],
   iamRoleStatementsInherit: true,
 };
--- services/payment/functions/handlePayment/handler.ts
+++ services/payment/functions/handlePayment/handler.ts
@@ -1,7 +1,8 @@
-import { applyHttpMiddlewares } from '@slsdemo/common';
+import { applyHttpMiddlewares, getEnvVariable } from '@slsdemo/common';
 import { PaymentAction } from 'libs/types/paymentAction';
 import { PaymentHandler, createPayment } from './paymentHandlers';
 import { FromSchema } from 'json-schema-to-ts';
+
 import inputSchema from './schema';
 
 type PaymentHandlersMap = Record<PaymentAction, PaymentHandler>;
@@ -10,10 +11,20 @@ const paymentHandlers: PaymentHandlersMap = {
   [PaymentAction.CREATE]: createPayment,
 };
 
+const ssmParameters = {
+  STRIPE_API_KEY: getEnvVariable('STRIPE_API_KEY_SSM_PATH'),
+};
+
 const handler = async ({ body }: FromSchema<typeof inputSchema>) => {
   const { action, ...receiptData } = body;
+
+  const stripeApiKey = getEnvVariable('STRIPE_API_KEY');
+  console.log({ stripeApiKey });
 
   return await paymentHandlers[action](receiptData);
 };
 
-export const main = applyHttpMiddlewares(handler);
+export const main = applyHttpMiddlewares(handler, {
+  inputSchema,
+  ssmParameters,
+});
